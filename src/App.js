28053cc25ec6b8e6a299543998ab932deaca64b0
MuhammadAbdullah <teachfreak.show@gmail.com>
2021-04-11 01:25:27 +0500

    Fectech Api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
+import Tour from './Tour';
 import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
+
 const url = 'https://course-api.com/react-tours-project'
-function App() {
-  return <h2>Tours Project Setup</h2>
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try{
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    }
+    catch(error){
+        setLoading(false);
+        console.log(error);
+    }
+
+  }
+ 
+
+  useEffect(() => {
+    fetchTours()
+  },[])
+  console.log(tours);
+  
+  if(loading){
+    return( 
+    <main>
+      <Loading />
+    </main>);
+  }
+
+  return ( 
+  <main>
+    <Tour tours={tours} />
+  </main>)
 }
 
 export default App
