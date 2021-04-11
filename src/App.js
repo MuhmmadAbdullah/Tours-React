3285fabd561166036bc00657c2b23e9c10d4d0e0
MuhammadAbdullah <teachfreak.show@gmail.com>
2021-04-11 16:28:41 +0500

    Fully Functional

--- src/App.js
+++ src/App.js
@@ -9,11 +9,18 @@ const App = () => {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter( (tour) => tour.id !== id);
+    setTours(newTours);
+  }
+
+
   const fetchTours = async () => {
     setLoading(true);
     try{
       const response = await fetch(url);
       const tours = await response.json();
+      console.log(tours);
       setLoading(false);
       setTours(tours);
     }
@@ -28,7 +35,7 @@ const App = () => {
   useEffect(() => {
     fetchTours()
   },[])
-  console.log(tours);
+
   
   if(loading){
     return( 
@@ -36,10 +43,17 @@ const App = () => {
       <Loading />
     </main>);
   }
-
+  if(tours.length === 0){
+    return <main>
+      <div className="title">
+        <h2>No Tours Left</h2>
+        <button className="btn" onClick={() => fetchTours()}>Get Tours List</button>
+      </div>
+    </main>
+  }
   return ( 
   <main>
-    <Tour tours={tours} />
+    <Tours tours={tours} removeTour={removeTour} />
   </main>)
 }
 
